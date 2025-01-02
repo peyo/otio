e0afeaa464686a052aca4c3d8b80dc55c9ad0185
peyo <peteryyoon@gmail.com>
2025-01-01 19:58:02 -0800

    Initial Commit

---
+++ Vibes/VibesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibesApp.swift
+//  Vibes
+//
+//  Created by Peter Yoon on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct VibesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VibesTests/VibesTests.swift
@@ -0,0 +1,17 @@
+//
+//  VibesTests.swift
+//  VibesTests
+//
+//  Created by Peter Yoon on 1/1/25.
+//
+
+import Testing
+@testable import Vibes
+
+struct VibesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
